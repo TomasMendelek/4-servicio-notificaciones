35dcc4c2fc1d879cd3ce89790739869f7cb5e5e0
TomasMendelek <tomascmendelek@gmail.com>
2024-10-31 16:23:22 -0300

    Initial commit

---
+++ src/main/java/com/backend/microservicioNumero4/MicroservicioN4Application.java
@@ -0,0 +1,13 @@
+package com.backend.microservicioNumero4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioN4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioN4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/microservicioNumero4/MicroservicioN4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.microservicioNumero4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioN4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
